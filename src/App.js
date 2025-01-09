ecd9ef85ac3ab0cf90b0ca4ca1c083c94a5b9195
avemunoori <85594271+avemunoori@users.noreply.github.com>
2025-01-08 23:18:51 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Sidebar from "./components/Sidebar";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Lessons from "./components/Lessons/Lessons";
+import LessonsList from "./components/Lessons/LessonsList"; // Updated import
 import Profile from "./components/Profile/Profile";
 import Quiz from "./components/Quiz/Quiz";
 import GrandmasterGames from "./components/GrandmasterGames/GrandmasterGames";
@@ -20,7 +20,7 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/lessons" element={<Lessons />} />
+            <Route path="/lessons" element={<LessonsList />} /> {/* Updated route */}
             <Route path="/profile" element={<Profile />} />
             <Route path="/quiz" element={<Quiz />} />
             <Route path="/grandmaster-games" element={<GrandmasterGames />} />
